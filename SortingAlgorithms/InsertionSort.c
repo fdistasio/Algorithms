66383ff1e600ab2b4d58decfc0d571b4854a382d
fdistasio <fdistasio02@gmail.com>
2023-10-19 18:10:12 +0200

    Create InsertionSort.c

---
+++ SortingAlgorithms/InsertionSort.c
@@ -0,0 +1,16 @@
+//Insertion Sort O(n^2)
+
+void insertionSort(int A[], int n){
+
+    int i = 0, j=0, k=0;
+    
+    for(j = 1 ; j < n ; j++){
+        k = A[j];
+        i = j-1;
+        while(i >= 0 && A[i] > k){
+            A[i+1] = A[i];
+            -- i;
+        }
+        A[i+1] = k;
+    }
+}
