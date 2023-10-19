22bf668f74338a5430b92dd025d352602b4d6ba4
fdistasio <fdistasio02@gmail.com>
2023-10-19 18:17:31 +0200

    Create BinarySearch.c

---
+++ ArraySearchingAlgorithms/BinarySearch.c
@@ -0,0 +1,18 @@
+//Binary Search O(logn)
+
+int binarySearch(int A[], int n, int k){
+    
+    int i = -1;
+    int left = 0;
+    int right = n-1;
+
+    while(left <= right && i == -1){
+        int mid = (left + right)/2;
+        if(A[mid] == k) 
+            i = mid;
+        else if(k < A[mid])
+            right = mid-1;
+        else
+            left = mid+1;
+    }
+}
