5d3f7215abbb7424a1c8b9d7e018aaf27100afe4
Francesco Di Stasio <francesco.distasio02@gmail.com>
2023-10-25 12:21:02 +0200

    Fixed BinarySearch.c

--- ArraySearchingAlgorithms/BinarySearch.c
+++ ArraySearchingAlgorithms/BinarySearch.c
@@ -8,11 +8,14 @@ int binarySearch(int A[], int n, int k){
 
     while(left <= right && i == -1){
         int mid = (left + right)/2;
-        if(A[mid] == k) 
+        if(A[mid] == k){
             i = mid;
+            return i;
+        }
         else if(k < A[mid])
             right = mid-1;
         else
             left = mid+1;
     }
+    return i;
 }
