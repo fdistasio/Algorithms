d9041ababb960209b2db4b1a4805aeab7587ff28
Francesco <francesco.distasio02@gmail.com>
2024-03-11 18:15:52 +0100

    Update macros.h

--- ProcessesAlgorithms/macros.h
+++ ProcessesAlgorithms/macros.h
@@ -1,3 +1,2 @@
-#define SYSC(value,command,message) if ((value=command) == -1){perror(message); exit(errno); }
-
-#define SYSCN(value,command,message) if ((value=command) == NULL){perror(message); exit(errno); }
+#define SYSC(value,command,message) if ((value = command) == -1){perror(message); exit(errno); }
+#define SYSCN(value,command,message) if ((value = command) == NULL){perror(message); exit(errno); }
